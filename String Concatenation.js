2df5b625aec7d04617acecf3698e174e936232e7
StatenIslandTech21 <71594701+StatenIslandTech21@users.noreply.github.com>
2020-09-23 16:19:24 -0400

    Accidently pushed to master instead of my second branch
    Fixed issue made before

--- String Concatenation.js
+++
@@ -1,7 +0,0 @@
-const Greet = "Hello";
-const Item = "World";
-const Times = Item + " " + 60 + " times"
-
-console.log(Greet + " " + Item);
-console.log(`${Greet}, I can also greet the ${Item} like this`);
-console.log(`When doing this I can Greet the ${Times}`)
