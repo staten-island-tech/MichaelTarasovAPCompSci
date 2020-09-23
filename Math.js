08b73faae565f6c1892004e50a25c04383e7f4c3
StatenIslandTech21 <71594701+StatenIslandTech21@users.noreply.github.com>
2020-09-23 17:59:55 -0400

    Functions

--- Math.js
+++ Math.js
@@ -1,1 +1,5 @@
-console.log(`I can make a random number like the guy in the video. ${Math.floor(Math.random() * 100 + 1)} is a random number`);
+console.log(
+  `I can make a random number like the guy in the video. ${Math.floor(
+    Math.random() * 100 + 1
+  )} is a random number`
+);
